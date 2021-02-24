8d3a420b9b81f5ff0f7e44f8470bbabe6029b47f
belajar <fahriadird@gmail.com>
2021-02-24 16:30:07 +0700

    project_laravel

---
+++ resources/views/biodata.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <title>Selamat Datang</title>
+  </head>
+  <body>
+
+	  	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container">
+    <a class="navbar-brand" href="index.php">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/biodata">Biodata</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br>
+  <div class="container text-center">
+    <h1 class="display-4">Fahriadi Rahma Dhany</h1>
+    
+  </div>
+
+<br>
+<br>
+	<div class="container">
+		<div class="row">
+		<div class="col text-center">
+			<h1>About Me</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col text-center">
+			<p>Nama saya Fahriadi Rahma Dhany. Saya lahir tanggal 14 Juli tahun
+			1999. Saya tinggal di daerah Jakarta timur. Hobi saya yaitu bermain game online dan basket. </p>
+		</div>
+		<div class="col text-center">
+			<p>Sekarang saya kuliah di Universitas Gunadarma mengambil jurusan Sistem Informasi. Saat ini saya sudah sampai di Semester 7 akhir dan akan masuk
+      Semester 8. Dan nanti saya akan merancang Skripsi, semoga di dalam pembuatannya saya di lancarkan dan di mudahkan. AMINN</p>
+		</div>
+</div>
+
+
+    
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
